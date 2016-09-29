ed04c14fa5fea6a71a7e014c0a460fab242d9222
Whitney Smith <whitney@gallerywrappeddesign.com>
2016-09-29 11:57:03 -0400

    Added geolocation with default coordinates.

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,27 @@ import Listing from './Listing'
 
 class App extends Component {
 
+  constructor () {
+    super()
+
+    this.state = { coordinates: {latitude: 27.7702504, longitude: -82.636237} }
+
+    window.onload = function () {
+      let startPos
+      let geoSuccess = function (position) {
+        startPos = position
+        this.state = { coordinates: startPos.coords }
+        console.log(this.state.coordinates)
+      }
+      navigator.geolocation.getCurrentPosition(geoSuccess.bind(this))
+    }
+  }
+
   render () {
     return (
             <div>
-            <Listing />
+            <p></p>
+            <Listing coordinates={this.state.coordinates}/>
             </div>
           )
   }

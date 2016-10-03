cedb6a2f669b72c8936ec940f3bd2eb7e1a4c4d6
Whitney Smith <whitney@gallerywrappeddesign.com>
2016-10-03 17:37:58 -0400

    React Router Set to Load Component Scroll To Top

--- src/components/Location.js
+++ src/components/Location.js
@@ -9,8 +9,8 @@ class Location extends Component {
   }
 
   render () {
+    // Get Listing from JSON based on URL Slug
     const { coordinates } = this.props
-    //console.log(coordinates)
     const listing = listings.allLocations.find((listing) => {
       return listing.Slug === this.props.params.Slug
     })
@@ -26,9 +26,8 @@ class Location extends Component {
           <h3><b>Amenities: </b>{listing.ActivitiesAmenities}</h3><h3><b>Hours: </b>{listing.Hours} | <b>Entrance/Parking Fee: </b>{listing.EntranceFee}</h3>
           <h3><a href={listing.Website}>Visit Website</a> <a href={listing.Phone}>Call Phone</a></h3>
           <p>{listing.Description}</p>
-          <iframe className='map'
-src={`https://www.google.com/maps/embed/v1/place?q=${listing.AddressLat},${listing.AddressLong}&key=AIzaSyCKH6qV66K3QlCU5d-Xr3tX9uEbBE6AQ_o`}></iframe>
           <a href={`https://www.google.com/maps/dir/${coordinates.latitude},${coordinates.longitude}/${listing.AddressLat},${listing.AddressLong}`} target="_blank"><button type='button' className='directions'>Open Directions</button></a>
+          <iframe className='map' src={`https://www.google.com/maps/embed/v1/place?q=${listing.AddressLat},${listing.AddressLong}&key=AIzaSyCKH6qV66K3QlCU5d-Xr3tX9uEbBE6AQ_o`}></iframe>
           </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Listing from './components/Listing'
 import { Router, Route, hashHistory } from 'react-router'
 
 render((
-  <Router history={hashHistory}>
+  <Router history={hashHistory} onUpdate={() => window.scrollTo(0, 0)}>
   <Route component={App}>
     <Route path='/' component={Listing} />
     <Route path='/location/:Slug' component={Location} />
